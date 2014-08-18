874c7ac21af0a880fad28706d73fc09f01f491d1
jsimpson <jared.simpson@gmail.com>
2014-08-18 15:00:13 -0400

    automatically download latest dbSNP

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -6,3 +6,10 @@ git clone https://github.com/samtools/bcftools.git
 git clone https://github.com/samtools/htslib.git
 git clone --recursive https://github.com/jts/bam2fastq.git
 git clone --recursive https://github.com/ekg/freebayes.git
+
+# Download dbsnp
+mkdir dbsnp
+cd dbsnp
+wget ftp://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606/VCF/00-All.vcf.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606/VCF/00-All.vcf.gz.tbi
+cd ..
